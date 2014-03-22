426afeb385e46e7e32841427c7c52e7c5631a4c5
Michael Wieland <github@selfcoders.com>
2014-03-22 02:16:54 +0100

    Some Travis debugging

--- tools/compile-gamemode.sh
+++ tools/compile-gamemode.sh
@@ -4,10 +4,20 @@ set -e
 
 TOOLSPATH=$(dirname $0)
 
+echo "TOOLSPATH = $TOOLSPATH"
+
 pushd $TOOLSPATH/../gamemodes > /dev/null
 
+pwd
+ls -la
+
 export LD_LIBRARY_PATH="../tools/pawn:$LD_LIBRARY_PATH"
+
+echo "LD_LIBRARY_PATH = $LD_LIBRARY_PATH"
 
 ../tools/pawn/pawncc grgserver.pwn -i../includes -\; -\(
 
 popd > /dev/null
+
+pwd
+ls -la
