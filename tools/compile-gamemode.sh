13da0e17a885a95875aee87a80c16dc3e98bbc26
Michael Wieland <github@selfcoders.com>
2014-03-22 02:00:42 +0100

    Fixed not always finding libpawnc.so

--- tools/compile-gamemode.sh
+++ tools/compile-gamemode.sh
@@ -4,9 +4,9 @@ set -e
 
 TOOLSPATH=$(dirname $0)
 
-export LD_LIBRARY_PATH="$TOOLSPATH/pawn:$LD_LIBRARY_PATH"
-
 pushd $TOOLSPATH/../gamemodes > /dev/null
+
+export LD_LIBRARY_PATH="../tools/pawn:$LD_LIBRARY_PATH"
 
 ../tools/pawn/pawncc grgserver.pwn -i../includes -\; -\(
 
