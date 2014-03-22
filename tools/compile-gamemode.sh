5aa920d9d013e3ec8ebca9c31b14fe3093b54ac5
Michael Wieland <github@selfcoders.com>
2014-03-22 02:55:47 +0100

    Removed debugging stuff

--- tools/compile-gamemode.sh
+++ tools/compile-gamemode.sh
@@ -8,9 +8,6 @@ export LD_LIBRARY_PATH="../tools/pawn:$LD_LIBRARY_PATH"
 
 pushd $TOOLSPATH/../gamemodes > /dev/null
 
-file ../tools/pawn/pawncc
-ldd ../tools/pawn/pawncc
-
 ../tools/pawn/pawncc grgserver.pwn -i../includes -\; -\(
 
 popd > /dev/null
