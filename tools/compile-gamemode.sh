ba561ddddc8381631f2768d10dd28ff24a70f2ba
Michael Wieland <github@selfcoders.com>
2014-03-22 02:19:29 +0100

    Travis debugging
    There is no other way...

--- tools/compile-gamemode.sh
+++ tools/compile-gamemode.sh
@@ -15,6 +15,11 @@ export LD_LIBRARY_PATH="../tools/pawn:$LD_LIBRARY_PATH"
 
 echo "LD_LIBRARY_PATH = $LD_LIBRARY_PATH"
 
+ls -la ..
+ls -la ../tools
+ls -la ../tools/pawn
+ls -la ../tools/pawn/pawncc
+
 ../tools/pawn/pawncc grgserver.pwn -i../includes -\; -\(
 
 popd > /dev/null
