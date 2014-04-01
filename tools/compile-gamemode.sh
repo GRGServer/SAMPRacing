af29aaec8671c6c044c31ffdd06b1f6dfa84462f
Michael Wieland <github@selfcoders.com>
2014-04-02 01:43:25 +0200

    Updated scripts to use new Includes Updater

--- tools/compile-gamemode.sh
+++ tools/compile-gamemode.sh
@@ -6,6 +6,10 @@ TOOLSPATH=$(dirname $0)
 
 export LD_LIBRARY_PATH="../tools/pawn:$LD_LIBRARY_PATH"
 
+$TOOLSPATH/apache-maven/bin/mvn -f $TOOLSPATH/includesupdater/pom.xml clean package
+
+java -jar $TOOLSPATH/includesupdater/target/includesupdater.jar
+
 pushd $TOOLSPATH/../gamemodes > /dev/null
 
 ../tools/pawn/pawncc grgserver.pwn -i../includes -\; -\(
