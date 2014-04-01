794955a4ef930f2b6edb3c6823ce8e569d5b6c50
Michael Wieland <github@selfcoders.com>
2014-04-02 00:57:37 +0200

    Added Apache Maven to tools
    Used to build for tools soon

---
+++ tools/includesupdater/src/main/java/net/grgserver/includesupdater/IncludeWriter.java
@@ -0,0 +1,8 @@
+package net.grgserver.includesupdater;
+
+/**
+ * Created by Programie on 4/2/2014.
+ */
+public class IncludeWriter
+{
+}
---
+++ tools/includesupdater/src/main/java/net/grgserver/includesupdater/Main.java
@@ -0,0 +1,12 @@
+package net.grgserver.includesupdater;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Programie
+ * Date: 4/2/14
+ * Time: 12:06 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main
+{
+}
