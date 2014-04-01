1ce49fd849608eab5853f71a4de5eafceb367eff
Michael Wieland <github@selfcoders.com>
2014-04-02 01:31:51 +0200

    Added new Includes Updater written in Java

--- tools/includesupdater/src/main/java/net/grgserver/includesupdater/IncludeWriter.java
+++ tools/includesupdater/src/main/java/net/grgserver/includesupdater/IncludeWriter.java
@@ -1,8 +1,75 @@
 package net.grgserver.includesupdater;
 
-/**
- * Created by Programie on 4/2/2014.
- */
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class IncludeWriter
 {
+	private final FileWriter fileWriter;
+	private final String fullIncludesPath;
+	private final String subPath;
+	private boolean lineWritten;
+
+	public IncludeWriter(String includesPath, String subPath, String mainIncludeFile) throws IOException
+	{
+		this.fullIncludesPath = includesPath + File.separator + subPath;
+
+		System.out.println("Updating includes in '" + this.fullIncludesPath + "'...");
+
+		this.fileWriter = new FileWriter(this.fullIncludesPath + File.separator + mainIncludeFile);
+		this.subPath = subPath;
+	}
+
+	public void addGroup(String name) throws IOException
+	{
+		// Add an empty line if this is not the first line
+		if (this.lineWritten)
+		{
+			this.writeLine("");
+		}
+
+		this.writeLine("// " + name);
+	}
+
+	public void addInclude(String name) throws IOException
+	{
+		this.writeLine("#include <" + (this.subPath + File.separator + name).replace("\\", "/") + ">");
+	}
+
+	public void addDirectory(String path) throws IOException
+	{
+		File directory = new File(this.fullIncludesPath + File.separator + path);
+
+		if (!directory.isDirectory())
+		{
+			return;
+		}
+
+		File[] files = directory.listFiles();
+
+		Arrays.sort(files);
+
+		this.addGroup(path);
+
+		for (int index = 0; index < files.length; index++)
+		{
+			if (files[index].isFile())
+			{
+				this.addInclude(path + File.separator + files[index].getName());
+			}
+		}
+	}
+
+	public void writeLine(String string) throws IOException
+	{
+		this.lineWritten = true;
+		this.fileWriter.write(string + "\n");
+	}
+
+	public void close() throws IOException
+	{
+		this.fileWriter.close();
+	}
 }
--- tools/includesupdater/src/main/java/net/grgserver/includesupdater/Main.java
+++ tools/includesupdater/src/main/java/net/grgserver/includesupdater/Main.java
@@ -1,12 +1,36 @@
 package net.grgserver.includesupdater;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Programie
- * Date: 4/2/14
- * Time: 12:06 AM
- * To change this template use File | Settings | File Templates.
- */
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+
 public class Main
 {
+	public static void main(String[] args) throws IOException
+	{
+		String serverPath = Paths.get(new File(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath()).getAbsolutePath()).getParent().getParent().getParent().getParent().toString();
+
+		IncludeWriter includesWriter = new IncludeWriter(serverPath + File.separator + "includes", "grgserver", "main.inc");
+
+		includesWriter.addGroup("Defines");
+		includesWriter.addInclude("localconfig.inc");
+		includesWriter.addInclude("constants.inc");
+		includesWriter.addInclude("macros.inc");
+
+		includesWriter.addDirectory("structures");
+
+		includesWriter.addGroup("Global variables");
+		includesWriter.addInclude("globals.inc");
+		includesWriter.addInclude("forwards.inc");
+
+		includesWriter.addDirectory("functions");
+		includesWriter.addDirectory("callbacks");
+		includesWriter.addDirectory("dialogs");
+		includesWriter.addDirectory("timers");
+		includesWriter.addDirectory("commands");
+
+		includesWriter.close();
+
+		System.out.println("Done");
+	}
 }
