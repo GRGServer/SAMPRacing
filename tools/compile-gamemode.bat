a386ecdd422180d8608b1e134634cc671bbf3a22
Michael Wieland <github@selfcoders.com>
2014-04-15 00:02:54 +0200

    Fixed not working jumping to file in Notepad++
    Pawncc output was relative to the directory containing the pwn file

--- tools/compile-gamemode.bat
+++ tools/compile-gamemode.bat
@@ -2,10 +2,17 @@
 
 set TOOLSPATH=%~dp0
 set SERVERPATH=%TOOLSPATH%..\
+
+call :normalise_server_path "%SERVERPATH%"
+
 set PAWNCC=%TOOLSPATH%pawn\pawncc.exe
 
 java -jar %TOOLSPATH%includesupdater\target\includesupdater.jar
 
-%PAWNCC% grgserver.pwn -D%SERVERPATH%gamemodes -i../includes -; -(
+%PAWNCC% %SERVERPATH%gamemodes\grgserver.pwn -i%SERVERPATH%includes -; -(
 
-%PAWNCC% npcplayer.pwn -D%SERVERPATH%npcmodes -i../includes -; -(
+%PAWNCC% %SERVERPATH%npcmodes\npcplayer.pwn -i%SERVERPATH%includes -; -(
+
+:normalise_server_path
+SET "SERVERPATH=%~f1"
+GOTO :EOF
